0c2db3ff8513bf627ad261008202002cd00e687a
adeandreass <adeandreass03@gmai.com>
2024-06-02 18:36:26 +0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+   <section className="text-3xl flex min-h-screen justify-center items-center">
+    <h2 className="font-semibold">LearnTum.</h2>
+   </section>
+  );
+}
