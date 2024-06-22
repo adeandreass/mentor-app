e66872abd92c3a1590fe0fbf7cef07b591654fc6
adeandreass <adeandreass03@gmai.com>
2024-06-22 09:46:51 +0700

    modern responsive dashboard

--- app/(back)/layout.tsx
+++ app/(back)/layout.tsx
@@ -1,17 +1,15 @@
-import Navbar from '@/components/Dashboard/NavBar'
-import Sidebar from '@/components/Dashboard/Sidebar'
-import React, { ReactNode } from 'react'
+import NavBar from "@/components/Dashboard/NavBar";
+import Sidebar from "@/components/Dashboard/Sidebar";
+import React, { ReactNode } from "react";
 
-export default function Layout({ children }: {children:ReactNode}) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <Navbar />
-      <div className='flex'>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
-      <div className="p-8">
-      {children}
-      </div>
+      <div className="flex flex-col">
+        <NavBar />
+        <div className="flex min-h-screen w-full flex-col">{children}</div>
       </div>
     </div>
-  )
+  );
 }
